47bbd6cd1f47922d6ffd881a5f65b7daea7af3b6
SanDiego <sandiegocastilho@gmail.com>
2018-01-17 17:47:02 -0300

    Ajuste

--- Lista 02/Python/questao_1_b_AumentoSalario.py
+++
@@ -1,8 +0,0 @@
-Salario = float(input("Digite o valor de seu salário: R$ "))
-
-if Salario < 900:
-    Salario = Salario + (Salario * 0.3)
-    print("Você receberá agora: R$",Salario)
-    
-else:
-    print("Você não merece um aumento!")
